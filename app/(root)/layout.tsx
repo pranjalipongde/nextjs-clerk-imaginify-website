553b801115157a65b9d26968297872bf744ad2c5
pranjalipongde09 <pranjalipongde9@gmail.com>
2024-05-09 00:28:25 +0530

    add image form(React hook form)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import MobileNav from "@/components/share/MobileNav";
-import Sidebar from "@/components/share/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
